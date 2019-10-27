b95886e93334e34e39b1d1ce65023dd37da5ad80
tgergo1 <gergely.toth@passr.by>
2019-10-27 17:55:13 +0100

    logo

---
+++ evariste.cpp
@@ -0,0 +1,8 @@
+
+#include <iostream>
+
+#include "lobe.h"
+
+int main(int argc, char ** argv){
+    return 0;
+}
---
+++ misc/misc.h
@@ -0,0 +1,11 @@
+#ifndef MISC
+#define MISC
+
+namespace misc{
+    class vector{
+    public:
+      double x,y,z;  
+    };
+};
+
+#endif
